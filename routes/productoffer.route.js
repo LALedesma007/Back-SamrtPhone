105d845044d4a5ba87e2177e138dd67b5b537e3d
LALedesma007 <laledesma@hotmail.com.ar>
2023-05-25 08:47:32 -0300

    Se modificaron validaciones en las rutas

--- routes/productoffer.route.js
+++ routes/productoffer.route.js
@@ -8,9 +8,9 @@ route.get('/getproductoffer', getAllProductOffer)
 
 route.get('/getproductofferbyid/:id', getProductOfferId)
 
-route.post('/createproductoffer', validateCreateProduct, jwtValidator, createAllProductOffer)
+route.post('/createproductoffer', validateCreateProduct, createAllProductOffer)
 
-route.patch('/editproductoffer/:id', validateCreateProduct, jwtValidator, editProductOffer )
+route.patch('/editproductoffer/:id', validateCreateProduct, editProductOffer )
 
 route.delete('/deleteproductoffer/:id', deleteProductOffer )
 
